b937865f8792900ded2c427a066a69fa89481fd7
Thomas Nitsche <thomas.nitsche@gmail.com>
2011-06-19 15:57:27 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mojito::Application.config.secret_token = 'f49adf8142a190d8e2e254e8437b7afa472cb79e37a13eb37871f06f9ad6f5badebe4dbb1f080dfbfcc16abe77ddc070f532816c04ed2dd03d29dbf9223b1d85'
